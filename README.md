c5136423892485ddfd4fd456ce5c73b5887aecf1
hardwyrd <hardwyrd@gmail.com>
2024-06-20 12:20:41 +0800

    Initial QCards commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# QCards 
+
+QCards is a very simple Python script that will generate QSL cards in bulk. At the moment, QCards accepts a CSV file and will generate the QSL cards for you.
+
+QCards is configurable via a simple configuration file. Customize QCards by setting the following customizable items:
+
+* BACKGROUND_IMAGE - your background image template. It can be JPEG or PNG or whatever.
+* BIG_FONT - the font that will be used to render the callsign.
+* BIG_FONT_SIZE - the size of the callsign font in pixels.
+* BIG_FONT_COLOR - the font color (eg. #ffffff).
+* SMALL_FONT - the font that will be used to render the extra details text about the QSO.
+* SMALL_FONT_SIZE - the size of the details font in pixels.
+* SMALL_FONT_COLOR - the font color for the details text.
+
+### License
+ This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+## Installation
+
+To install QCards, you can clone this Git repository or download the artifacts.
+`git clone https://hardwyrd@bitbucket.org/hardwyrd/qcards.git`
+
+# Running QCards
+
+## Requirements
+
+Since QCards is a Python script, it is much better to provide QCards with a virtual environment. I personally just use Python 3's own virtual env module (venv). You can feel free to use whatever method you prefer, be it pyenv, pipenv, conda, and others. It's all perfectly fine ^_^. 
+
+Go ahead and create the virtual environment and activate:
+
+`python3 -m venv qcards_env`
+`source qcards_env/bin/activate`
+
+Install the required Python modules via the requirements.txt file:
+`pip install -r requirements.txt`
+
+## Configuration
+
+The repository includes a sample config.sample file. Feel free to copy config.sample to config.ini and modify and update config.ini as you see fit.
+
+The QCards repository DOES NOT INCLUDE fonts that you can use to generate your QSL cards. However, you can feel free to download free and open source TrueType fonts anywhere on the web. Make sure to set in your config.ini the path of the fonts that QCards will use. Ideally, you can put the font files within the QCards directory/folder.
+
+## Template Image
+
+To make things way simpler, QCards uses an image template which will serve as your background image for the generated QSL cards. QCards will only insert the callsign, and the call details. All other elements that you would like to include in your QSL card design like the logos, background colors, extra background images and other design elements will need to be modified by you using your favorite image editor and prepare prior. 
+
+By default, QCards expects that your background image/template is 1280 pixels wide, and 720 pixels high. If you are going to use images with different dimensions, make sure to update config.ini under the [DEFAULT] section and modify the following:
+
+```
+[DEFAULT]
+CARD_W=1280
+CARD_H=720
+```
+
+## CSV Data
+
+QCards accepts data from CSV with the following format:
+
+`Callsign, Handle, Date & Time, Mode, RST`
+
+Ex.
+```
+4g9lbz,Romar,06/19/2024 20:00:00 Asia/Manila,ASL,5/9
+```
+
+The included sample netcall_logs.csv.sample follows this format. Feel free to copy or rename netcall_logs.csv.sample to netcall_logs.csv and update with your own data.
+
+## Syntax
+
+To generate QSL cards using the default configuration, run
+`./qcards.py`
+
+To generate QSL cards using a custom configuration, run 
+`./qcards.py -c <your config file>`
+
+To generate QSL cards from a custom CSV file, run
+`./qcards.py -f <your CSV file>`
+
+To generate QSL cards from a custom configuration and custom CSV file, run
+`./qcards.py -c <your config file> -f <your CSV file>
+
+### Support
+If you like this little tool, you can buy me coffee ^_^ and thank you for your support. 
+https://buymeacoffee.com/ninerlbz
---
+++ qcards.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+#
+# QCards
+# hardwyrd.at.gmail.dot.com
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from PIL import Image, ImageDraw, ImageFont
+from configparser import ConfigParser
+import click
+import csv
+
+@click.command()
+@click.option('-c', '--configuration_file', help='Custom config file')
+@click.option('-f', '--csv_file', help='CSV file')
+def generate_card(configuration_file, csv_file):
+
+    config = ConfigParser()
+
+    # Parse config
+    if configuration_file:
+        config.read(configuration_file)
+    else:
+        config.read('config.ini')
+    
+    # Load CSV file
+    if csv_file:
+        call_logs = csv_file
+    else:
+        call_logs = config['DEFAULT']['CSV_FILE']
+
+    with open(call_logs, newline='') as netcall:
+        callers = csv.reader(netcall, delimiter=',')
+
+        for caller in callers:
+            # We run everything under the for loop kay para ma-instantiate ug bag-o 
+            # nga Image/ImageDraw kay if dili fresh instantiation, magsapaw-sapaw ang 
+            # injected texts once mag-call na ug Image.save() later.
+
+            # Default card dimensions
+            # We pick up the card dimensions from config.ini 
+            W, H = (config['DEFAULT']['CARD_W'],config['DEFAULT']['CARD_H'])
+
+            # Default font position height coords (in pixels)
+            # Gi-set ni sila statically dinhi kay dili man ga-change ang height
+            # sa injected texts. We only need to make sure nga naka-center ang 
+            # texts later.
+            cs_h = 276
+            de_h = 520
+
+            # Load settings into variables
+            # Ako lang ni gi-ingon ani kay para dili kaayo taas ang i-reference 
+            # nga variables. Kapoy type! ^_^
+            background_image = config['SETTINGS']['BACKGROUND_IMAGE']
+            cs_fontface = config['SETTINGS']['BIG_FONT']
+            cs_fontsize = config['SETTINGS']['BIG_FONT_SIZE']
+            cs_fontcolor = config['SETTINGS']['BIG_FONT_COLOR']
+            de_fontface = config['SETTINGS']['SMALL_FONT']
+            de_fontsize = config['SETTINGS']['SMALL_FONT_SIZE']
+            de_fontcolor = config['SETTINGS']['SMALL_FONT_COLOR']
+
+            # Load the QSL card template image
+            # Mao pud ni nga object atong ipa-save later.
+            qcard_bg = Image.open(background_image)
+
+            # Initialize the draw method
+            qcard = ImageDraw.Draw(qcard_bg)
+
+            # Set up CS fontface and size
+            cs_font = ImageFont.truetype(cs_fontface, int(cs_fontsize))
+
+            # Set up details fontface and size
+            de_font = ImageFont.truetype(de_fontface, int(de_fontsize))
+
+            # Construct the fields
+            # Ako ra ni gi-humanize para dali ma-ilhan kay kapoy
+            # mag-psycho sa list items ug list item id :D
+            # Technically, gi-slice ra nato ang list item. Naay lain way nga
+            # maka-wow pero the simpler the better.
+            cs = caller[0]
+            handle = caller[1]
+            datetime = caller[2]
+            mode = caller[3]
+            rst = caller[4]
+
+            # Prep the qsl card details format
+            details = f'Name: {handle} | Date/Time: {datetime} | Mode: {mode} | RST: {rst}'
+
+            # Get the text length so we can properly center the text
+            cs_w = qcard.textlength(cs.upper(), font=cs_font)
+            de_w = qcard.textlength(details, font=de_font)
+
+            # Inject the texts and generate the qsl card image
+            print(f'Generating QSL card for {cs.upper()} ...')
+
+            # Things to note lang. We need to make sure to cast the numbers to int()
+            # para dili mabuang sa calculation.
+            # Where: qcard.text((X coordinate, Y coordinate), text, font, fontcolor)
+            qcard.text(((int(W) - int(cs_w))/2, cs_h), cs.upper(), font=cs_font, fill=cs_fontcolor)
+            qcard.text(((int(W) - int(de_w))/2, de_h), details, font=de_font, fill=cs_fontcolor)
+            qcard_filename = f"{cs}.png"
+            qcard_bg.save(qcard_filename)
+
+if __name__ == '__main__':
+    generate_card()
